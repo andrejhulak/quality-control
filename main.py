1a48bf363938f5de78035ff6bfdb75394a3f22ea
andrejhulak <115270326+andrejhulak@users.noreply.github.com>
2024-11-20 15:02:21 +0100

    attention_heatmap v1

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import torch
 from vit_pytorch import ViT
 from vit_pytorch.cct import CCT
 from scripts.model import *
+from vit_pytorch.recorder import Recorder
+from vit_pytorch.extractor import Extractor
+
+# use segmentation techniques
 
 # remember that neurala did it for first 500 and first 100
 
@@ -22,38 +26,6 @@ if __name__ == '__main__':
   dl_train = DataLoader(ds_train, batch_size=BATCH_SIZE, shuffle=True)
   dl_test = DataLoader(ds_test, batch_size=BATCH_SIZE, shuffle=False)
 
-  # image_size = 64
-  # patch_size = 2
-  # num_classes = 2
-  # dim = 2
-  # depth = 2
-  # heads = 2
-  # mlp_dim = 128
-  # dropout = 0.1
-  # emb_dropout = 0.1
-
-  # model_config = {
-  #   'image_size' : image_size,
-  #   'patch_size' : patch_size,
-  #   'num_classes' : num_classes,
-  #   'dim' : dim,
-  #   'depth' : depth,
-  #   'heads' : heads,
-  #   'mlp_dim' : mlp_dim,
-  #   'dropout' : dropout,
-  #   'emb_dropout' : emb_dropout
-  #   }
-
-  # model = ViT(image_size = image_size,
-  #             patch_size = patch_size,
-  #             num_classes = num_classes,
-  #             dim = dim,
-  #             depth = depth,
-  #             heads = heads,
-  #             mlp_dim = mlp_dim,
-  #             dropout = dropout,
-  #             emb_dropout = emb_dropout)
-            
   model = CCT(
     img_size = (64, 64),
     embedding_dim = 32,
@@ -71,24 +43,26 @@ if __name__ == '__main__':
     positional_embedding = 'learnable', # ['sine', 'learnable', 'none']
 )
           
-  pytorch_total_params = sum(p.numel() for p in model.parameters())
-  print(pytorch_total_params)
+  # pytorch_total_params = sum(p.numel() for p in model.parameters())
+  # print(pytorch_total_params)
 
-  optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
-  loss_fn = torch.nn.CrossEntropyLoss()
+  # optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
+  # loss_fn = torch.nn.CrossEntropyLoss()
   
-  results = train(model=model,
-                  train_dataloader=dl_train,
-                  test_dataloader=dl_test,
-                  optimizer=optimizer,
-                  loss_fn=loss_fn,
-                  epochs=num_epochs,
-                  device='cpu')
+  # results = train(model=model,
+  #                 train_dataloader=dl_train,
+  #                 test_dataloader=dl_test,
+  #                 optimizer=optimizer,
+  #                 loss_fn=loss_fn,
+  #                 epochs=num_epochs,
+  #                 device='cpu')
 
   # y_true = 
 
-  y_pred = test_step(model, dl_test, loss_fn=loss_fn, device='cpu')
+  model = Recorder(model)
+  test = torch.randn(1, 3, 64, 64) 
+  result, attention = model(test)
+  print(attention.shape)
 
-  print(y_pred)
+  # y_pred = test_step(model, dl_test, loss_fn=loss_fn, device='cpu')
 
-  # save_models(model=model, model_config=model_config, save_dir='models/test')
